bf13b8f6fef9f93302e17011fb35823c431b617b
PartyTime11 <e.barzdun@yandex.ru>
2024-10-06 22:47:21 +1000

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,11 @@
-/* eslint-env node */
-import '@rushstack/eslint-patch/modern-module-resolution'
-export default {
-  env: {
-    browser: true,
-    node: true
-  },
-  root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  rules: {
-    'vue/multi-word-component-names': 0,
-    semi: ['error', 'always']
-  },
-  parserOptions: {
-    ecmaVersion: 'latest'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 0,
+      semi: ['error', 'always']
+    }
   }
-}
+]
